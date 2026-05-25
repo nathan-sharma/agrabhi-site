53bd559f4fc21e5028b22000aaedc90d0c34a0a6
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2026-05-25 08:58:59 +0000

    browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+// 1. Changed HashRouter to BrowserRouter
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import DataHub from "./DataHub";
 import "leaflet/dist/leaflet.css";
 import About from "./About";
-import Updates from "./Updates"
+import Updates from "./Updates";
 
 export default function App() {
   return (
-    <HashRouter>
+    // 2. Added basename so paths map correctly to your GitHub repository URL
+    <BrowserRouter basename="/agrabhi-website">
       <Routes>
         {/* Main page */}
         <Route path="/" element={<Home />} />
 
         {/* DataHub page */}
         <Route path="/data-hub" element={<DataHub />} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/updates" element={<Updates/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/updates" element={<Updates />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
