a6fd5af07ca07651619707c7755c5c3ae123a570
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2026-05-27 01:50:30 +0000

    .com

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path' // Required to locate the physical HTML files
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/agrabhi-website/",
+  base: "/",
   build: {
     rollupOptions: {
       input: {
