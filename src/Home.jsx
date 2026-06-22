68f75a6aa42339ab74723d66b58e9ef4ca6c425a
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2026-06-22 03:15:54 +0000

    new youtube video of overview

--- src/Home.jsx
+++ src/Home.jsx
@@ -201,7 +201,7 @@ export default function Home() {
          <div className="mt-6 relative aspect-video rounded-xl overflow-hidden border border-slate-800 bg-black w-[70%] h-relative">
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/i61x3b99Wog"
+              src="https://www.youtube.com/embed/6o-V2qKVF9o"
               title="AgraBhi 2027 Overview Presentation by Nathan"
               frameBorder="0"
               allowFullScreen
